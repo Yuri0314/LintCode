499599c58767c72453d2c825a6774e68e75f2c1c
xtl4350 <ix22824953@sina.com>
2020-08-07 14:23:44 +0800

    67. 二叉树的中序遍历

---
+++ ProblemSet/67. 二叉树的中序遍历/67. 二叉树的中序遍历 递归法.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Definition of TreeNode: public class TreeNode { public int val; public
+ * TreeNode left, right; public TreeNode(int val) { this.val = val; this.left =
+ * this.right = null; } }
+ */
+
+public class Solution {
+    private List<Integer> ans = new ArrayList<Integer>();
+    /**
+     * @param root: A Tree
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        traverse(root);
+        return ans;
+    }
+
+    public void traverse(TreeNode root) {
+        if (root != null) {
+            inorderTraversal(root.left);
+            ans.add(root.val);
+            inorderTraversal(root.right);
+        }
+    }
+}
---
+++ ProblemSet/67. 二叉树的中序遍历/67. 二叉树的中序遍历 非递归法.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition of TreeNode: public class TreeNode { public int val; public
+ * TreeNode left, right; public TreeNode(int val) { this.val = val; this.left =
+ * this.right = null; } }
+ */
+
+public class Solution {
+    /**
+     * @param root: A Tree
+     * @return: Inorder in ArrayList which contains node values.
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>(); 
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            if (!stack.isEmpty()) {
+                root = stack.pop();
+                ans.add(root.val);
+                root = root.right;
+            }
+        }
+        return ans;
+    }
+}
