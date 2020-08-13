1e338aa80a9f77ceb01fb9dfeca1c89a6a3f7e22
xtl4350 <ix22824953@sina.com>
2020-08-13 12:08:30 +0800

    fix

--- ProblemSet/104. 合并k个排序链表/104. 合并k个排序链表 优先队列法.java
+++ ProblemSet/104. 合并k个排序链表/104. 合并k个排序链表 优先队列法.java
@@ -2,7 +2,6 @@
  * 设总结点数为N，链表数为k，则时间复杂度为O(N*logk)
  * 设每个链表最长长度为n，链表数为k，则时间复杂度为O(nk*logk)
  */
-import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -19,18 +18,13 @@ import java.util.Queue;
  * }
  */ 
 public class Solution {
-    private Comparator<ListNode> listNodeComparator = new Comparator<ListNode>() {
-        public int compare(ListNode node1, ListNode node2) {
-            return node1.val - node2.val;
-        }
-    };
     /**
      * @param lists: a list of ListNode
      * @return: The head of one sorted list.
      */
     public ListNode mergeKLists(List<ListNode> lists) {  
         if (lists == null || lists.size() == 0) return null;
-        Queue<ListNode> heap = new PriorityQueue<ListNode>(lists.size(), listNodeComparator);
+        Queue<ListNode> heap = new PriorityQueue<ListNode>(lists.size(), (node1, node2) -> node1.val - node2.val);
         for (ListNode node : lists) {
             if (node != null) heap.add(node);
         }
