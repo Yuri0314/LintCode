8b7472766c9de3d06d67387f1179119ef110ca9f
xtl4350 <ix22824953@sina.com>
2020-09-21 15:05:29 +0800

    1891. 旅行计划

---
+++ ProblemSet/1891. 旅行计划/1891. 旅行计划 dp——迭代.java
@@ -0,0 +1,23 @@
+public class Solution {
+    /**
+     * @param arr: the distance between any two cities
+     * @return: the minimum distance Alice needs to walk to complete the travel plan
+     */
+    public int travelPlan(int[][] arr) {
+        int n = arr.length;
+        int m = (1 << (n - 1));
+        int[][] dp = new int[n][m];
+        for (int i = 1; i < n; ++i) dp[i][0] = arr[i][0];
+        for (int j = 1; j < m; ++j) {
+            for (int i = 0; i < n; ++i) {
+                dp[i][j] = Integer.MAX_VALUE;
+                if (((j >> (i - 1)) & 1) == 1) continue;    // 如果集合包括源点则跳过
+                for (int k = 1; k < n; ++k) {   // 对集合依次遍历所有结点
+                    if (((j >> (k - 1)) & 1) == 1)  // 集合存在该结点
+                        dp[i][j] = Math.min(dp[i][j], arr[i][k] + dp[k][j ^ (1 << (k - 1))]);
+                }
+            }
+        }
+        return dp[0][m - 1];
+    }
+}
---
+++ ProblemSet/1891. 旅行计划/1891. 旅行计划 dp——递归.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+
+public class Solution {
+    private int[][] arr;
+    private int[][] memo;
+    private int n;
+    private int m;
+    /**
+     * @param arr: the distance between any two cities
+     * @return: the minimum distance Alice needs to walk to complete the travel plan
+     */
+    public int travelPlan(int[][] arr) {
+        this.arr = arr;
+        n = arr.length;
+        m = (1 << (n - 1));
+        memo = new int[n][m];
+        for (int i = 0; i < n; ++i)
+            Arrays.fill(memo[i], Integer.MAX_VALUE);
+        return search(0, m - 1);
+    }
+
+    private int search(int src, int set) {
+        if (set == 0) return arr[src][0];   // 如果此时集合为空，直接返回src到0结点的距离
+        if (memo[src][set] != Integer.MAX_VALUE) return memo[src][set];
+        for (int i = 1; i < n; ++i) {   // 遍历除了目标点0之外的每个结点
+            if (((set >> (i - 1)) & 1) == 1)    // 如果集合中有该结点
+                memo[src][set] = Math.min(memo[src][set], arr[src][i] + search(i, set ^ (1 << (i - 1))));
+        }
+        return memo[src][set];
+    }
+}
