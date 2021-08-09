2d0334ddebef8c0eea0c3a0b7f0f20fe1328ef30
陆洵 <huitianyu.hty@alibaba-inc.com>
2021-08-09 14:40:34 +0800

    464 · 整数排序 II 堆排序

---
+++ ProblemSet/464 · 整数排序 II/java/464 · 整数排序 II 堆排序.java
@@ -0,0 +1,36 @@
+public class Solution {
+    /**
+     * @param A: an integer array
+     * @return: nothing
+     */
+    public void sortIntegers2(int[] A) {
+        heapSort(A);
+    }
+
+    private void heapSort(int[] nums) {
+        for (int i = (nums.length >> 1) - 1; i >= 0; --i)
+            maxHeapify(nums, i, nums.length);
+        for (int i = nums.length - 1; i > 0; --i) {
+            swap(nums, 0, i);
+            maxHeapify(nums, 0, i);
+        }
+    }
+
+    private void maxHeapify(int[] nums, int root, int len) {
+        int tmp = nums[root];
+        while ((root << 1) + 1 < len) {
+            int child = (root << 1) + 1;
+            if (child + 1 != len && nums[child] < nums[child + 1]) ++child;
+            if (tmp >= nums[child]) break;
+            else nums[root] = nums[child];
+            root = child;
+        }
+        nums[root] = tmp;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
